650f26f5a0011c37762433fe7dd2a2d583865a8a
menyasosali <tapakah9@inbox.ru>
2023-08-03 11:37:53 +0300

    add cobfig

--- config/config.go
+++ config/config.go
@@ -1,7 +1,22 @@
 package config
 
 type Config struct {
-	MongoDB MongoDB
+	Redis    RedisConfig
+	RabbitMQ RabbitMQ
+	Logger   Logger
+	MongoDB  MongoDB
+}
+
+type RabbitMQ struct {
+	Host           string
+	Port           string
+	User           string
+	Password       string
+	Exchange       string
+	Queue          string
+	RoutingKey     string
+	ConsumerTag    string
+	WorkerPoolSize int
 }
 
 type MongoDB struct {
@@ -9,4 +24,20 @@ type MongoDB struct {
 	User     string
 	Password string
 	DB       string
+}
+
+type Logger struct {
+	Level string
+}
+
+type RedisConfig struct {
+	RedisAddr      string
+	RedisPassword  string
+	RedisDB        string
+	RedisDefaultDB string
+	MinIdleConn    int
+	PoolSize       int
+	PoolTimeout    int
+	Password       string
+	DB             int
 }
